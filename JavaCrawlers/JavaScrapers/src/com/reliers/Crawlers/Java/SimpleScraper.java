f7f1e79aef8a2854d61b9abcac8d3f931c6015c2
srinath <americakart@gmail.com>
2015-06-17 23:23:50 +0530

    init commit

---
+++ JavaCrawlers/JavaScrapers/src/com/reliers/Crawlers/Java/SimpleScraper.java
@@ -0,0 +1,30 @@
+package com.reliers.Crawlers.Java;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author reliers
+ */
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.net.*;
+import java.io.*;
+
+public class SimpleScraper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
